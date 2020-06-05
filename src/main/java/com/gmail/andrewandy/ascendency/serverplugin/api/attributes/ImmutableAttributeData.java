34c8ead0d33a5d5a71312fb3c6d34f7d87f93d8b
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-06-05 17:01:50 +0200

    Fix merge conflicts

---
+++ src/main/java/com/gmail/andrewandy/ascendency/serverplugin/api/attributes/ImmutableAttributeData.java
@@ -0,0 +1,18 @@
+package com.gmail.andrewandy.ascendency.serverplugin.api.attributes;
+
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.api.data.manipulator.ImmutableDataManipulator;
+import org.spongepowered.api.data.value.BoundedValue;
+import org.spongepowered.api.data.value.immutable.ImmutableBoundedValue;
+import org.spongepowered.api.data.value.mutable.MutableBoundedValue;
+
+public interface ImmutableAttributeData
+    extends ImmutableDataManipulator<ImmutableAttributeData, AttributeData> {
+
+    @NotNull ImmutableBoundedValue<Integer> getAttribute(@NotNull AscendencyAttribute attribute);
+
+    default int getAttributePrimitive(@NotNull AscendencyAttribute attribute) {
+        final BoundedValue<Integer> boundedValue = getAttribute(attribute);
+        return boundedValue.exists() ? attribute.defaultValue() : boundedValue.get();
+    }
+}
